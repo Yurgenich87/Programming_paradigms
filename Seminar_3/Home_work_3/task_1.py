8855ddbb5a0dcebb5c8ee6901b16c4b8f79019e6
Yurgenich <yurakarbushev@gmail.com>
2023-11-17 19:56:33 +0700

    Add home work 3

---
+++ Seminar_3/Home_work_3/task_1.py
@@ -0,0 +1,61 @@
+def check_winner(array, player):
+    # Условия победы для строк, столбцов и диагоналей
+    win_conditions = [
+        [[0, 0], [0, 1], [0, 2]],  # Верхний ряд
+        [[1, 0], [1, 1], [1, 2]],  # Средний ряд
+        [[2, 0], [2, 1], [2, 2]],  # Нижний ряд
+        [[0, 0], [1, 0], [2, 0]],  # Левый столбец
+        [[0, 1], [1, 1], [2, 1]],  # Средний столбец
+        [[0, 2], [1, 2], [2, 2]],  # Правый столбец
+        [[0, 0], [1, 1], [2, 2]],  # Диагональ слева направо
+        [[0, 2], [1, 1], [2, 0]]   # Диагональ справа налево
+    ]
+
+    for condition in win_conditions:
+        marks = [array[row][col] for row, col in condition]
+        if all(mark == player for mark in marks):
+            return True
+    return False
+
+
+def print_board(array):
+    for row in array:
+        print(' '.join(row))
+
+
+def play_game():
+    letters = [['-' for _ in range(3)] for _ in range(3)]
+    print_board(letters)
+
+    player = 'x'
+    while True:
+        print(f"Ход {player}:")
+        row_index = int(input("Введите номер строки(от 1 до 3): ")) - 1
+        column_index = int(input("Введите номер столбца(от 1 до 3): ")) - 1
+
+        if letters[row_index][column_index] == '-':
+            letters[row_index][column_index] = player
+        else:
+            print("Эта клетка уже занята. Попробуйте снова.")
+            continue
+
+        print_board(letters)
+
+        if check_winner(letters, player):
+            print(f"<<< Выйграли {player} >>>")
+            break
+
+        # Проверка на ничью
+        draw = all(mark != '-' for row in letters for mark in row)
+        if draw:
+            print("<<< Ничья >>>")
+            break
+
+        # Смена игрока
+        player = 'o' if player == 'x' else 'x'
+
+    print("Игра завершена.")
+
+
+if __name__ == "__main__":
+    play_game()
